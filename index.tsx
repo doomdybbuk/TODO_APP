2a2c2b0fd91a222bd044bda717e6126eb290b516
doomdybbuk <manan.jain16949@gmail.com>
2023-10-18 02:00:26 +0530

    Todo app

--- index.tsx
+++ index.tsx
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import { request } from "http";
+import { renderToString } from "react-dom/server"
+
+const server = Bun.serve({
+    hostname :"localhost",
+    port : 3000,
+    fetch: fetchHandler
+})
+
+console.log(`Bun server running on ${server.hostname}:${server.port}`);
+
+type Todo = {id:number; name:string};
+const todos:Todo[]=[];
+
+ function fetchHandler(request:Request):Response{
+    const url = new URL(request.url)
+
+    if (url.pathname === "" || url.pathname === "/"){
+        return new Response(Bun.file("index.html"));
+    }
+    if (url.pathname === "/todos" && request.method === "GET"){
+        return new Response (renderToString(<TodoList todos={todos}/>))
+    }
+    return new Response("Not Found",{status:404});
+ }
+ function TodoList(props:{todos:Todo[]}){
+    return<ul>
+        {props.todos.length ? props.todos.map(todo=><li>{todo.name}</li>)
+        :"No Todo Found"}
+    </ul>
+ }
